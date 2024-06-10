76e67b81a08fce06fce87edab2ad8079ed0be968
Storm2103 <snowstorm2013@gmail.com>
2024-06-09 21:23:59 -0600

    build

--- obj/Debug/net8.0/CalTestHelpers.AssemblyInfo.cs
+++ obj/Debug/net8.0/CalTestHelpers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalTestHelpers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+379526540bd210f372f842dbf0c9e90317d4b14e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b67cf63d0817defd29e56cb36499badb57d3dae")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalTestHelpers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalTestHelpers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
